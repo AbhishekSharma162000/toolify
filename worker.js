ee13e94d27171f8487994d27adfc5506376ca8d2
Abhishek Sharma <84061026+AbhishekSharma162000@users.noreply.github.com>
2026-03-17 23:25:04 +0530

    fo

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ export default {
 
     // always return index.html no matter what
     return env.ASSETS.fetch(
-      new Request(url.origin + "/index.html", request)
+      url.origin + "/index.html"
     );
   },
 };
