19c80d3e317af80b04a24bd8b9378db9849b2eac
Abhishek Sharma <84061026+AbhishekSharma162000@users.noreply.github.com>
2026-03-17 23:37:37 +0530

    fixed

--- worker.js
+++ worker.js
@@ -1,8 +1,5 @@
 export default {
   async fetch(request, env) {
-    const url = new URL(request.url);
-    const indexUrl = new URL("/index.html", url.origin);
-
-    return env.ASSETS.fetch(new Request(indexUrl.toString(), request));
-  },
-};
+    return env.ASSETS.fetch(request)
+  }
+}
